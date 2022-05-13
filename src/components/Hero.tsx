1b4b5ee6b374896747bb4f5d671aa1bca0728ca9
Alan Sanchez <realalansan@gmail.com>
2022-05-13 05:39:04 -0700

    Create planet animation

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,8 +1,38 @@
+import { useRef } from 'react';
 
 const Hero: React.FC = () => {
-return <div className="hero">
-    
-</div>;
+  const planet = useRef(null);
+
+  const loopPlanet = async () => {
+    setTimeout(() => {
+      return planet.current;
+    }, 1000);
+  };
+
+  return (
+    <div className="hero">
+      <div ref={planet} className="planet-1">
+        <div className="land-mass land-mass-1" />
+        <div className="land-mass land-mass-2" />
+        <div className="land-mass land-mass-3"></div>
+      </div>
+      <div className="hero-content">
+        <h4 className="hero-title">Outer News</h4>
+        <p className="hero-info">
+          Space articles fetched from{' '}
+          <span>
+            <a
+              target="_blank"
+              referrerPolicy="no-referrer"
+              href="https://api.spaceflightnewsapi.net/v3/documentation"
+            >
+              API
+            </a>
+          </span>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
